6598f05f8b90d17dccd38d86db96f2f4f7ebf620
NotMuuns <santicipo21@gmail.com>
2023-06-30 11:28:33 -0300

    Creo el componente de header e items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemsComponent } from './components/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
